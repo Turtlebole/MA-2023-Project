7da2ec368f215dfff366202e26647bb8b929055a
krist <krist@LAPTOP-NBTNJTVJ>
2023-04-22 16:42:31 +0200

    Dodat skocko, spojnice, asocijacije_window_xml izmenjen

---
+++ app/src/main/java/com/example/mobilneprojekat/Spojnice.java
@@ -0,0 +1,14 @@
+package com.example.mobilneprojekat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Spojnice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_spojnice_window);
+    }
+}
