0d5007e9188e966cf5f9923feffb0f64aa1834c7
Turtlebole <babic.bojan62@gmail.com>
2023-04-30 21:39:20 +0200

    dodat korak po korak i statistika

---
+++ app/src/main/java/com/example/mobilneprojekat/GameStaticticsActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobilneprojekat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameStaticticsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_game_statictics);
+    }
+}
