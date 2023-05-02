b1b42c9780bfdcf44d1bf6b5cc6d5dc44c881738
krist <krist@LAPTOP-NBTNJTVJ>
2023-05-02 02:36:14 +0200

    pusha za resolve problema

--- app/src/main/java/com/example/mobilneprojekat/WeeklyLeaderboard.java
+++ app/src/main/java/com/example/mobilneprojekat/WeeklyLeaderboard.java
@@ -2,7 +2,10 @@ package com.example.mobilneprojekat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WeeklyLeaderboard extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class WeeklyLeaderboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_weekly_leaderboard_window);
+
+        Button monthly_leaderboard = findViewById(R.id.monthly_leaderboard);
+        monthly_leaderboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to the next page
+                Intent intent = new Intent(WeeklyLeaderboard.this, MonthlyLeaderboard.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
