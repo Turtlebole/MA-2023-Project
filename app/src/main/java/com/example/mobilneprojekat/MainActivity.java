61cd15ad6b3c981ddb813e4397d18dd595aa4cdb
krist <krist@LAPTOP-NBTNJTVJ>
2023-05-02 03:52:33 +0200

    Buttoni koji rade

--- app/src/main/java/com/example/mobilneprojekat/MainActivity.java
+++ app/src/main/java/com/example/mobilneprojekat/MainActivity.java
@@ -1,37 +1,29 @@
 package com.example.mobilneprojekat;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.mobilneprojekat.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    private Button ProfilButon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.fragment_profile_window, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        ProfilButon = findViewById(R.id.ProfilButon);
+        ProfilButon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Go to the next page here
+                Intent intent = new Intent(MainActivity.this, Profile.class);
+                startActivity(intent);
+            }
+        });
     }
-
 }
--- app/src/main/java/com/example/mobilneprojekat/Profile.java
+++ app/src/main/java/com/example/mobilneprojekat/Profile.java
@@ -1,14 +1,30 @@
 package com.example.mobilneprojekat;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Profile extends AppCompatActivity {
+
+    private ImageView KoZnaZnaButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_profile_window);
+
+        KoZnaZnaButton = findViewById(R.id.KoZnaZnaButton);
+        KoZnaZnaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Go to the next page here
+                Intent intent = new Intent(Profile.this, KoZnaZna.class);
+                startActivity(intent);
+            }
+        });
     }
 }
